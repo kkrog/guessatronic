ebf2aa8b3205430ca7c7341ef4793882dc7aede1
kkrog <58795517+kkrog@users.noreply.github.com>
2020-04-17 15:51:25 -0500

    correct and incorrect guesses populating correctly

--- easyGame.js
+++ easyGame.js
@@ -1,14 +1,17 @@
 //Easy Level: all words 4 letters or less, no timer, wrong guesses same as twice letters in word;
+document.getElementById("timerWrap").style.visibility="hidden"; 
+/*OR just show infinite time
+document.getElementById("timer").textContent="∞";
+*/
 const easyList=["hall","page","raw","rib","city","draw","list","wolf","wife","raid","take","live","roof","save","make","mean","host","like","pray","chip","lay","acid","glow","art","snub","lack","boat","fork","cane","care","sigh","car","rare","map","sale","bat","bond","goal","mass","hurl","hill","wage","bait","rage","goat","land","pit","unit","ward","mind","burn","weak","van","rush","bang","axis","cast","file","find","last","vain","band","wall","tie","beg","turn","coma","load","fish","cute","rock","stay","bind","meat","gem","lace","riot","pole","eaux","feed","lung","good","fade","pen","cry","time","wash","toll","dark","wood","way","heel","law","pat","red","wake","nap","cell","moon","aid","arch","just","win","tip","soil","view","soak","flu","veil","soul","fax","pipe","nut","dive","fool","game","stem","jet","pig","rest","tape","silk","lid","hide","debt","job","mine","bank","mile","tidy","cow","ally","poll","slot","tree","bean","sail","brag","clay","desk","lazy","loan","poem","free","pest","era","tone","neck","stab","disk","fail","maze","pot","shop","hand","lead","log","slap","plan","tap","poor","main","act","wing","jail","race","joke","tear","lift","lie","loop","dump","top","hip","halt","belt","wave","deny","hurt","seed","heat","urge","age","loud","man","step","lean","lost","part","word","pawn","pull","mill","heir","navy","coal","date","fat","blue","set","peak","code","die","get","wind","node","pour","deer","shy","sin","nun","size"];
 let getWord=easyList[Math.floor(Math.random()*easyList.length)];
 let splitWord=getWord.split("");
 console.log(splitWord);
 let swL=splitWord.length;
-let guesses=new Array(2*swL);
-
-//let secretWord=[];
+let guesses=new Array(swL);
+let playCount=0;
 let populateSecretWord=()=>{
-    for(i=0;i<8;i++){
+    for(i=0;i<swL;i++){
         let secretLetter=document.createElement("span");
         secretLetter.textContent="_";
         secretLetter.setAttribute("class","ul nul spacey");
@@ -16,17 +19,31 @@ let populateSecretWord=()=>{
     }
 };
 populateSecretWord();
-/*let secretHolder=document.getElementById("goodGuesses");
-secretHolder.appendChild(secretWord);*/
-/*document.getElementById('goodGuesses').textContent;
+let wrongWrap=document.getElementById("wrongWrap");
 let wrongLetters=[];
 let guess=()=>{
     let gl=document.getElementById('guessBox').value;
     if(splitWord.includes(gl)){
-        guesses.splice(splitWord.indexOf(gl),1,gl);
-        
+        let letterloc=splitWord.indexOf(gl);
+        guesses.splice(letterloc,1,gl);
+        let rightLetter=document.createElement("span");
+        rightLetter.textContent=gl;
+        rightLetter.setAttribute("class","ul spacey");
+        let replaceIt=document.getElementById("goodGuesses").children[letterloc];
+        let replaceParent=document.getElementById("goodGuesses");
+        replaceParent.replaceChild(rightLetter,replaceIt);
+        playCount++;
+    } else {
+        wrongWrap.style.visibility="visible";
+        wrongLetters.push(gl);
+        let wrong=document.getElementById("wrong");
+        wrong.textContent+=gl;
+        playCount++;
+        //move robot to right by changing col class, barometer percent vs total moves
     }
-}*/
+}
+const guessButton=document.getElementById('guessButton');
+guessButton.onclick=guess;
 /*let letters=['h','o','u','n','d'];
 let lel=letters.length;
 let guesses=new Array(lel);
