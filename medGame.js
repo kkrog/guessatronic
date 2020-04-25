8e6e986a04856144da9970bee11381f3baba4273
kkrog <58795517+kkrog@users.noreply.github.com>
2020-04-25 18:09:03 -0500

    medium level finished

--- medGame.js
+++ medGame.js
@@ -13,11 +13,9 @@ let medPlay=()=>{
     outer.classList.add("align-items-xl-end");
     pauvre.classList.add("align-items-xl-start");
     smoke.classList.add("ml-xl-5");
-    const mediumList=["road","loose","serve","meat","flush","mill","sacred","flood","aspect","murder","news","bleed","nuance","low","share","wild","oven","duty","tiptoe","deadly","visual","win","belong","legend","tablet","feel","thank","opera","poison","assume","beef","full","shelf","social","damage","loop","panel","carrot","die","king","star","irony","ignore","method","fresh","worker","brake","volume","green","seek","late","diet","nest","cafe","jet","reward","crack","plain","disk","speed","cow","god","cute","dog","buy","upset","free","march","gravel","card","dine","tape","soar","eat","coup","front","woman","arena","employ","smash","load","oil","mood","oral","view","knot","agenda","hard","policy","home","guilt","accept","basin","tribe","value","dragon","graze","quiet","brave","twist","float","lawyer","able","fast","ghost","resort","plug","firm","decide","gift","order","tax","wake","pot","hay","depend","gutter","mark","result","inject","giant","habit","palm","Venus","voyage","rotten","boat","gas","bench","cheque","repeat","miss","west","drop","crude","warm","calm","self","velvet","ready","wound","useful","top","model","fork","punish","work","petty","pole","back","shorts","heavy","engine","stick","filter","water","middle","clean","brick","dilute","bet","branch","bus","form","hiccup","deep","refund","canvas","pipe","give","school","favour","punch","father","meal","draw","lily","weak","pepper","broken","option","ample","wood","breeze","sound","insert","wire","hole","gain","sweet","virus","facade","wonder","lion","risk","refuse","peanut","bottle","date","palace","oppose","trial","trance","cancel","world","wrong","injury","debut","ban","suntan","thanks","lift","writer","fly","fire","shine","prayer","gold","unit","bring","read","sense","slump","bolt","mole","tip","ritual","coffin","monk","ratio","age","wrist","final","queue","fraud","chop","drift","piece","mix"];
+    const mediumList=["road","loose","serve","meat","flush","mill","sacred","flood","aspect","murder","news","bleed","nuance","slow","share","wild","oven","duty","tiptoe","deadly","visual","wing","belong","legend","tablet","feel","thank","opera","poison","assume","beef","full","shelf","social","damage","loop","panel","carrot","diet","king","star","irony","ignore","method","fresh","worker","brake","volume","green","seek","late","diet","nest","cafe","jester","reward","crack","plain","disk","speed","scowl","geode","cute","doge","buyer","upset","free","march","gravel","card","dine","tape","soar","beat","coup","front","woman","arena","employ","smash","load","moil","mood","oral","view","knot","agenda","hard","policy","home","guilt","accept","basin","tribe","value","dragon","graze","quiet","brave","twist","float","lawyer","able","fast","ghost","resort","plug","firm","decide","gift","order","taxes","wake","post","fray","depend","gutter","mark","result","inject","giant","habit","palm","Venus","voyage","rotten","boat","gases","bench","cheque","repeat","miss","west","drop","crude","warm","calm","self","velvet","ready","wound","useful","stop","model","fork","punish","work","petty","pole","back","shorts","heavy","engine","stick","filter","water","middle","clean","brick","dilute","better","branch","bushy","form","hiccup","deep","refund","canvas","pipe","give","school","favour","punch","father","meal","draw","lily","weak","pepper","broken","option","ample","wood","breeze","sound","insert","wire","hole","gain","sweet","virus","facade","wonder","lion","risk","refuse","peanut","bottle","date","palace","oppose","trial","trance","cancel","world","wrong","injury","debut","bane","suntan","thanks","lift","writer","flyer","fire","shine","prayer","gold","unit","bring","read","sense","slump","bolt","mole","trip","ritual","coffin","monk","ratio","aged","wrist","final","queue","fraud","chop","drift","piece","mixer"];
     let getWord=mediumList[Math.floor(Math.random()*mediumList.length)];
     let splitWord=getWord.split("");
-    //for debugging gameplay - delete line 10 when everything works
-    console.log(splitWord);
     let swL=splitWord.length;
     let guesses=new Array(swL);
     let populateSecretWord=()=>{
@@ -29,11 +27,15 @@ let medPlay=()=>{
         }
     };
     populateSecretWord();
-    let wrongWrap=document.getElementById("wrongWrap");
-    wrongWrap.style.visibility="hidden";
+    cloud.style.visibility="hidden";
     let wrongLetters=[];
     let wc=0;
     let mp=swL;
+    if(mp==4){
+        cloud.classList.add("offset-1");
+        pauvre.classList.remove("offset-4");
+        pauvre.classList.add("offset-3");
+    };
     let gc=0;
     let ga=[];
     let win=false;
@@ -81,8 +83,7 @@ let medPlay=()=>{
                         rightLetter.classList.add("ul","spacey");
                         let replaceIt=document.getElementById("goodGuesses").children[c];
                         let replaceParent=document.getElementById("goodGuesses");
-                        replaceParent.replaceChild(rightLetter,replaceIt);  
-                        console.log(ga);
+                        replaceParent.replaceChild(rightLetter,replaceIt);
                     }
                 };
                 if((wc<=mp)&&(JSON.stringify(splitWord)===JSON.stringify(guesses))){
@@ -90,12 +91,7 @@ let medPlay=()=>{
                     win=true;}
             } else {
                 wc++;
-                wrongWrap.style.visibility="visible";
-                if(mp==4){
-                    wrongWrap.classList.add("offset-1");
-                    cloud.classList.remove("offset-4");
-                    cloud.classList.add("offset-3");
-                };
+                cloud.style.visibility="visible";
                 wrongLetters.push(gl);
                 let wrong=document.getElementById("wrong");
                 wrong.textContent+=gl;  
@@ -126,7 +122,7 @@ let medPlay=()=>{
                     const bgs1=new Audio('./assets/badGuessSound.mp3');
                     bgs1.play();
                     if(cloud.classList.contains(`offset-${wc-1}`)){
-                        cloud.classList.remove(`offst-${wc-1}`);
+                        cloud.classList.remove(`offset-${wc-1}`);
                         cloud.classList.add(`offset-${wc}`);
                         pauvre.classList.remove(`offset-${4-wc+1}`);
                         pauvre.classList.add(`offset-${4-wc}`);
@@ -136,7 +132,19 @@ let medPlay=()=>{
                         pauvre.classList.add(`offset-${4-wc}`);
                     }
                 } else {
-
+                    const bgs1=new Audio('./assets/badGuessSound.mp3');
+                    bgs1.play();
+                    if(pauvre.classList.contains(`offset-3`)){
+                        pauvre.classList.remove(`offset-3`);
+                        pauvre.classList.add(`offset-${Math.abs(wc-3)}`);
+                        cloud.classList.remove(`offset-1`);
+                        cloud.classList.add(`offset-${wc+1}`);
+                    } else{
+                        cloud.classList.remove(`offset-${wc}`);
+                        cloud.classList.add(`offset-${wc+1}`);
+                        pauvre.classList.remove(`offset-${wc}`);
+                        pauvre.classList.add(`offset-${wc-1}`);
+                    };
                 };
             };
             document.getElementById('guessBox').value="";
