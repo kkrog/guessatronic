8c00d4eabdb74aed7663ec6378f7c6b04080e5a5
kkrog <58795517+kkrog@users.noreply.github.com>
2020-04-26 21:00:26 -0500

    v 1.12 creature bug fix part 2

--- hardGame.js
+++ hardGame.js
@@ -220,7 +220,7 @@ let hardPlay=()=>{
                 document.getElementById('gameOver').style.display="block";
                 document.getElementById('guessButton').style.display="none";
                 if(win===false){
-                    document.getElementById("cage").style.backgroundImage=`url(${picked})`;
+                    document.getElementById("cage").style.backgroundImage=`url(${saveable})`;
                     document.getElementById("gameFin").style.display="block";
                     document.getElementById('showWord').textContent=getWord;
                     pauvre.style.visibility="hidden";
